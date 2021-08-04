f7d8dab09d9cee4a90265e7c04aa8eab01119816
Kevin Tooley <kevin.tooley@snhu.edu>
2021-08-03 20:07:53 -0400

    Initial Commit

---
+++ MyLibrary/MyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryApp.swift
+//  MyLibrary
+//
+//  Created by Kevin Tooley on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
